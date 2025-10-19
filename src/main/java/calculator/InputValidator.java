5955cadcf45142199e40903db088a05e0598accf
[sadew1112] <[bbg380027@gmail.com]>
2025-10-19 14:23:25 +0900

    fix : InputValidator 에서 \\n이 커스텀 구분자 입력값으로 들어오는 경우 예외처리

--- src/main/java/calculator/InputValidator.java
+++ src/main/java/calculator/InputValidator.java
@@ -11,10 +11,26 @@ public class InputValidator {
             if (!input.contains("\\n")) {
                 throw new IllegalArgumentException("커스텀 구분자 형식에 맞지 않습니다.");
             }
+
+            if (count(input) > 1) {
+                throw new IllegalArgumentException("\\n은 구분자로 사용하실 수 없습니다.");
+            }
         }else{
             if (!input.matches("^[0-9,:]*$")) {
                 throw new IllegalArgumentException(", 와 : 를 제외한 다른 문자가 포함되어 있습니다.");
             }
         }
+    }
+
+    private int count(String input) {
+        String sep = "\\n";
+        int count = 0;
+
+        while (input.indexOf(sep) != -1) {
+            int index = input.indexOf(sep);
+            input = input.substring(index + 2);
+            count++;
+        }
+        return count;
     }
 }
--- src/test/java/calculator/InputValidatorTest.java
+++ src/test/java/calculator/InputValidatorTest.java
@@ -5,23 +5,23 @@ import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
 
 public class InputValidatorTest {
     private final InputValidator validator = new InputValidator();
 
     @Test
     void 정상_실행_예시1() {
-        assertThatThrownBy(() -> validator.validate("2,3;4"))
-                .doesNotThrowAnyException();
+        assertThatNoException()
+                .isThrownBy(() -> validator.validate("2,3:4"));
     }
 
     @Test
     void 정상_실행_예시2() {
-        assertThatThrownBy(() -> validator.validate("//;\\n1;3"))
-                .doesNotThrowAnyException();
+        assertThatNoException()
+                .isThrownBy(() -> validator.validate("//;\\n1;3"));
     }
+
 
     @Test
     void null_예외발생() {
@@ -31,6 +31,12 @@ public class InputValidatorTest {
     @Test
     void 음수_포함시_예외발생() {
         assertThatThrownBy(() -> validator.validate("//1,2,3"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 커스텀구분자_예외발생() {
+        assertThatThrownBy(() -> validator.validate("//\\n\\n\\n1,2,3"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
